dc8ecc253f8e8bf1e5886a761c476c1291a45971
JohanLi2333 <k20020910@kcl.ac.uk>
2023-03-23 13:34:25 +0000

    setting update

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,8 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(
+          bufnr) end)
       end,
       desc = "Pick to close",
     },
@@ -20,6 +21,13 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- copilot
+    -- imap <silent><script><expr> <C-J> copilot#Accept("\<CR>")
+    -- let g:copilot_no_tab_map = v:true
+    -- let g:copilot_no_enter_map = v:true
+    ["<C-J>"] = { "<cmd>lua require('copilot').accept('<CR>')<CR>", desc = "Copilot" },
+    ["<C-K>"] = { "<cmd>lua require('copilot').accept('<BS>')<CR>", desc = "Copilot" },
   },
   t = {
     -- setting a mapping to false will disable it
