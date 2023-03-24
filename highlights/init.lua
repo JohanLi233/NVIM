e2d58a49f62c04af210c9181d5b4b8c43c0503bf
JohanLi2333 <k20020910@kcl.ac.uk>
2023-03-24 12:12:23 +0000

    setting update

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,16 @@
 return { -- this table overrides highlights in all themes
   -- Normal = { bg = "#005280" },
+  require("notify").setup({
+    stages = "fade_in_slide_out",
+    timeout = 5000,
+    background_colour = "#005280",
+    position = "top_right",
+    icons = {
+      ERROR = " ",
+      WARN = " ",
+      INFO = " ",
+      DEBUG = " ",
+      TRACE = " ",
+    },
+  }),
 }
