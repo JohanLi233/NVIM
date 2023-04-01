b7b80690290200727ad4663d13738f426c812012
JohanLi2333 <k20020910@kcl.ac.uk>
2023-04-01 20:50:15 +0100

    update

--- mappings.lua
+++ mappings.lua
@@ -23,6 +23,9 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- grep
+    ["<leader>fg"] = { "<cmd>lua require('telescope.builtin').live_grep()<cr>", desc = "Live grep" },
   },
   t = {
     -- setting a mapping to false will disable it
