58f422e53476baa911cb92f0686a5c4e69c7c6f1
JohanLi2333 <k20020910@kcl.ac.uk>
2023-08-02 11:36:21 +0100

    update

--- plugins/coq.lua
+++
@@ -1,4 +0,0 @@
-return {
-  lazy = true,
-  'ms-jpq/coq_nvim',
-}
--- plugins/leap.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ggandor/leap.nvim",
-  lazy = false,
-  priority = 150,
-  config = function()
-    require("leap").add_default_mappings()
-  end,
-}
