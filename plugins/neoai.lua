cb49f1be8aaeb4070db312ed7b2b225a0517fb5a
JohanLi2333 <k20020910@kcl.ac.uk>
2023-08-08 20:33:27 +0100

    Title: Adds NeoAI plugin for code generation
    Description: This commit adds a new plugin, "neoai.lua", which provides functionality
     for code generation using NeoAI. It includes dependencies for "nui.nvim" and
     adds several commands and key mappings for convenient code generation. Additionally
    , a configuration function is implemented for customizing options.

---
+++ plugins/neoai.lua
@@ -0,0 +1,27 @@
+return {
+    "Bryley/neoai.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    cmd = {
+        "NeoAI",
+        "NeoAIOpen",
+        "NeoAIClose",
+        "NeoAIToggle",
+        "NeoAIContext",
+        "NeoAIContextOpen",
+        "NeoAIContextClose",
+        "NeoAIInject",
+        "NeoAIInjectCode",
+        "NeoAIInjectContext",
+        "NeoAIInjectContextCode",
+    },
+    keys = {
+        { "<leader>ag", desc = "generate git message" },
+    },
+    config = function()
+        require("neoai").setup({
+            -- Options go here
+        })
+    end,
+}
